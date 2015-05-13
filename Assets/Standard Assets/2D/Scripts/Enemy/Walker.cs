e587c6fd59333f343d98050fcf90a19e9e97f784
overmindz <iqmathematics@gmail.com>
2015-05-13 15:40:23 +0700

    add enemy - walker

---
+++ Assets/Standard Assets/2D/Scripts/Enemy/Walker.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walker : MonoBehaviour {
+
+	public int direction;
+	public float speed;
+	float defaultHeadLocalPosX;
+	float defaultBumperLocalPosX;
+
+	// Use this for initialization
+	void Start () {
+		GameObject player = GameObject.Find ("CharacterRobotBoy");
+		foreach(Collider2D collider in player.GetComponents<Collider2D>()){
+			foreach(Collider2D enemyCollider in GetComponents<CircleCollider2D>()){
+				Physics2D.IgnoreCollision(collider, enemyCollider);
+
+			}
+		}
+		Transform head = transform.FindChild ("Head");
+		defaultHeadLocalPosX = Mathf.Abs (head.localPosition.x);
+		Transform bumper = transform.FindChild ("Bumper");
+		defaultBumperLocalPosX = Mathf.Abs (bumper.localPosition.x);
+		setObjectFromDirection ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Walk ();
+	}
+
+	void Walk(){
+		transform.position += new Vector3 (speed * direction, 0f, 0f);
+	}
+
+	public void ChangeDirection(){
+		direction *= -1;
+		setObjectFromDirection ();
+	}
+	void setObjectFromDirection(){
+		Transform head = transform.FindChild ("Head");
+		head.localPosition = new Vector3 (defaultHeadLocalPosX * direction, head.localPosition.y, head.localPosition.z);
+		Transform bumper = transform.FindChild ("Bumper");
+		bumper.localPosition = new Vector3 (defaultBumperLocalPosX * direction, bumper.localPosition.y, bumper.localPosition.z);
+	}
+
+
+}
---
+++ Assets/Standard Assets/2D/Scripts/Enemy/WalkerBody.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkerBody : MonoBehaviour {
+
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "Enemy" || other.gameObject.layer == 8) {
+			transform.parent.GetComponent<Walker>().ChangeDirection();
+		}
+	}
+}
