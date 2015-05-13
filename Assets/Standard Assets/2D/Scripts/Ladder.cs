077ca7703e087548c3cb37ffbfdb47837860b288
AbatrozzProgrammingSpace <abatrozz@hotmail.com>
2015-05-13 15:31:22 +0700

    add lot of stuff

---
+++ Assets/Standard Assets/2D/Scripts/Ladder.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets._2D;
+
+public class Ladder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D ( Collider2D other ) {
+		if ( other.tag == "Player" ) {
+			other.GetComponent<PlatformerCharacter2D>().onLadder = true;
+		}
+	}
+
+	void OnTriggerExit2D ( Collider2D other ) {
+		if ( other.tag == "Player" ) {
+			other.GetComponent<PlatformerCharacter2D>().onLadder = false;
+		}
+	}
+}
---
+++ Assets/Standard Assets/2D/Scripts/LadderTop.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets._2D;
+
+public class LadderTop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D ( Collider2D other ) {
+		if ( other.tag == "Player" ) {
+			other.GetComponent<PlatformerCharacter2D>().onTopLadder = true;
+		}
+	}
+
+	void OnTriggerExit2D ( Collider2D other ) {
+		if ( other.tag == "Player" ) {
+			other.GetComponent<PlatformerCharacter2D>().onTopLadder = false;
+		}
+	}
+}
