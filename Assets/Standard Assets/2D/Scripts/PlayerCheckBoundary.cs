b7f4a7f7f6f48f25bd18f775cc29ed19dfcd8b0d
overmindz <iqmathematics@gmail.com>
2015-05-14 14:45:22 +0700

    Add player check bounary - disable all child object when player is not in range

---
+++ Assets/Standard Assets/2D/Scripts/PlayerCheckBoundary.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCheckBoundary : MonoBehaviour {
+
+	bool isActive;
+
+	// Use this for initialization
+	void Start () {
+		isActive = false;
+		EnableObject (false);
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (!isActive) {
+			isActive = true;
+			EnableObject(true);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if (isActive) {
+			isActive = false;
+			EnableObject(false);
+		}
+	}
+
+	void EnableObject(bool enable){
+		print ("set object " + enable);
+		foreach (Transform child in transform.parent.transform) {
+			if(child != transform)
+				child.gameObject.SetActive(enable);
+		}
+	}
+
+}
