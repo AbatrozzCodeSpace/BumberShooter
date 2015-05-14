eeadbf985b2411550b0bca3a989604df74f4cebd
overmindz <iqmathematics@gmail.com>
2015-05-14 11:06:22 +0700

    add mode to turret

--- Assets/Standard Assets/2D/Scripts/Turret/GunTurretAttack.cs
+++ Assets/Standard Assets/2D/Scripts/Turret/GunTurretAttack.cs
@@ -7,7 +7,7 @@ public class GunTurretAttack : MonoBehaviour {
 	public float cooldownTime;
 	public float rotateAngleStart;
 	public float rotateAngleEnd;
-	public bool dualSide;
+	public bool dualMode;
 	float cooldownTimer = 0;
 	float attackTimer = 0;
 	int direction = 1;
@@ -15,25 +15,47 @@ public class GunTurretAttack : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		RotateGun (direction, 0);
+		isActive = false;
+		EnableGun (false);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		attackTimer += Time.deltaTime;
-		if (attackTimer > attackTime) {
-			attackTimer = 0;
-			direction *= -1;
+		if (isActive) {
+			attackTimer += Time.deltaTime;
+			if (attackTimer > attackTime) {
+				attackTimer = 0;
+				direction *= -1;
+				isActive = false;
+				EnableGun(false);
+			}
+			else{
+				RotateGun(direction,attackTimer/attackTime);
+			}
+		} else {
+			cooldownTimer += Time.deltaTime;
+			if (cooldownTimer > cooldownTime){
+				cooldownTimer = 0;
+				isActive = true;
+				EnableGun(true);
+				if(!dualMode){
+					direction *= -1;
+				}
+			}
+			else{
+				if(!dualMode)
+					RotateGun(direction, cooldownTimer/cooldownTime);
+			}
 		}
-		RotateGun (direction);
 	}
 
-	void RotateGun(int direction){
+	void RotateGun(int direction, float progress){
 		if (direction > 0) {
-			transform.localRotation = Quaternion.Slerp (Quaternion.Euler (new Vector3 (0, 0, rotateAngleStart)), Quaternion.Euler (new Vector3 (0, 0, rotateAngleEnd)), attackTimer / attackTime);
+			transform.localRotation = Quaternion.Slerp (Quaternion.Euler (new Vector3 (0, 0, rotateAngleStart)), Quaternion.Euler (new Vector3 (0, 0, rotateAngleEnd)), progress);
 		} else {
 			if (direction < 0){
-				transform.localRotation = Quaternion.Slerp (Quaternion.Euler (new Vector3 (0, 0, rotateAngleEnd)), Quaternion.Euler (new Vector3 (0, 0, rotateAngleStart)), attackTimer / attackTime);
+				transform.localRotation = Quaternion.Slerp (Quaternion.Euler (new Vector3 (0, 0, rotateAngleEnd)), Quaternion.Euler (new Vector3 (0, 0, rotateAngleStart)), progress);
 			}
 		}
 		WaterGenerator waterScript = GetComponentInChildren<WaterGenerator> ();
@@ -43,6 +65,10 @@ public class GunTurretAttack : MonoBehaviour {
 		float nowAngle = transform.localRotation.eulerAngles.z;
 		print ("ss " + nowAngle+" " + (nowAngle / 180 * Mathf.PI) + " " +Mathf.Sin(nowAngle/180*Mathf.PI)+" "+Mathf.Cos(nowAngle/180*Mathf.PI));
 		GetComponentInChildren<WaterGenerator> ().particleForce = new Vector3(-speed*Mathf.Sin(nowAngle/180*Mathf.PI), speed*Mathf.Cos(nowAngle/180*Mathf.PI),0);
+	}
+
+	void EnableGun(bool enable){
+		GetComponentInChildren<WaterGenerator> ().enabled = enable;
 	}
 
 
