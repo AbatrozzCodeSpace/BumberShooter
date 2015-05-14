896cdfad3fc0cdcc9b94cb80db052eba0ecd9e82
AbatrozzProgrammingSpace <abatrozz@hotmail.com>
2015-05-14 14:14:34 +0700

    shooting water will decreasing the water lv

--- Assets/Standard Assets/2D/Scripts/Medkit.cs
+++ Assets/Standard Assets/2D/Scripts/Medkit.cs
@@ -3,13 +3,45 @@ using System.Collections;
 
 public class Medkit : MonoBehaviour {
 
+	public float healing = 0.25f;
+	public bool pickable;
+
 	// Use this for initialization
 	void Start () {
-	
+		if ( pickable ) {
+			GetComponent<BoxCollider2D>().isTrigger = true;
+		} else {
+			GetComponent<BoxCollider2D>().isTrigger = true;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnTriggerEnter2D( Collider2D other ) {
+		if ( pickable ) {
+			if ( other.gameObject.tag == "Player" ) {
+				other.GetComponent<CharacterHealth>().heal( healing );
+				// TODO spawn picking effect
+				Destroy ( gameObject );
+
+			}
+		} else {
+			if ( other.gameObject.tag == "Blow" ) {
+				FindObjectOfType<CharacterHealth>().heal( healing );
+				// TODO spawn picking effect
+				Destroy ( gameObject );
+				
+			}
+		}
+	}
+
+	void OnCollisionEnter2D( Collision2D other ) {
+		if ( other.gameObject.tag == "Blow" ) {
+			FindObjectOfType<CharacterHealth>().heal( healing );
+			Destroy ( gameObject );
+		}
 	}
 }
