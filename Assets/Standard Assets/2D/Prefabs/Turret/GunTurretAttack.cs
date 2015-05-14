1ead8b32d77620195c121c533304173ed058cddf
overmindz <iqmathematics@gmail.com>
2015-05-14 09:26:17 +0700

    fix walker enemy add turret

---
+++ Assets/Standard Assets/2D/Prefabs/Turret/GunTurretAttack.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunTurretAttack : MonoBehaviour {
+
+	public float attackTime;
+	public float cooldownTime;
+	public float rotateAngleStart;
+	public float rotateAngleEnd;
+	public bool dualSide;
+	float cooldownTimer = 0;
+	float attackTimer = 0;
+	int direction = 1;
+	bool isActive;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		attackTimer += Time.deltaTime;
+		if (attackTimer > attackTime) {
+			attackTimer = 0;
+			direction *= -1;
+		}
+		RotateGun (direction);
+	}
+
+	void RotateGun(int direction){
+		if (direction > 0) {
+			transform.localRotation = Quaternion.Slerp (Quaternion.Euler (new Vector3 (0, 0, rotateAngleStart)), Quaternion.Euler (new Vector3 (0, 0, rotateAngleEnd)), attackTimer / attackTime);
+		} else {
+			if (direction < 0){
+				transform.localRotation = Quaternion.Slerp (Quaternion.Euler (new Vector3 (0, 0, rotateAngleEnd)), Quaternion.Euler (new Vector3 (0, 0, rotateAngleStart)), attackTimer / attackTime);
+			}
+		}
+		WaterGenerator waterScript = GetComponentInChildren<WaterGenerator> ();
+		float speed = waterScript.particleForce.magnitude;
+		//print ("speed " + speed);
+
+		float nowAngle = transform.localRotation.eulerAngles.z;
+		print ("ss " + nowAngle+" " + (nowAngle / 180 * Mathf.PI) + " " +Mathf.Sin(nowAngle/180*Mathf.PI)+" "+Mathf.Cos(nowAngle/180*Mathf.PI));
+		GetComponentInChildren<WaterGenerator> ().particleForce = new Vector3(-speed*Mathf.Sin(nowAngle/180*Mathf.PI), speed*Mathf.Cos(nowAngle/180*Mathf.PI),0);
+	}
+
+
+
+
+
+
+}
