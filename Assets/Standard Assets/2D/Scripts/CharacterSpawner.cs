325f57873bd17a800227b4591bc582ad1b625c72
AbatrozzProgrammingSpace <abatrozz@hotmail.com>
2015-05-16 01:32:05 +0700

    add checkpoint system

---
+++ Assets/Standard Assets/2D/Scripts/CharacterSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSpawner : MonoBehaviour {
+
+	//public GameObject spawnPosition;
+
+	void Awake() {
+		//DontDestroyOnLoad( spawnPosition );
+	}
+
+	// Use this for initialization
+	void Start () {
+		if( PlayerPrefs.HasKey( "SpawnX" ) ) {
+			transform.position = new Vector3( PlayerPrefs.GetFloat( "SpawnX" ), PlayerPrefs.GetFloat( "SpawnY" ) );
+
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnApplicationQuit() {
+		PlayerPrefs.DeleteKey("SpawnX");
+		PlayerPrefs.DeleteKey("SpawnY");
+	}
+}
---
+++ Assets/Standard Assets/2D/Scripts/Checkpoint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+
+	public GameObject spawnPoint;
+
+	void Awake() {
+		DontDestroyOnLoad( spawnPoint );
+	}
+
+	void OnTriggerEnter2D( Collider2D other ) {
+		if( other.tag == "Player" ) {
+//			other.GetComponent<CharacterSpawner>().spawnPosition = gameObject;
+			PlayerPrefs.SetFloat( "SpawnX", gameObject.transform.position.x );
+			PlayerPrefs.SetFloat( "SpawnY", gameObject.transform.position.y );
+			Destroy ( gameObject.GetComponent<ParticleSystem>() );
+
+		}
+	}
+
+}
